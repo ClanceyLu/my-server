8afe1e5cb027c10ada5205916bb8d5999beb0c34
ClanceyLu <ClanceyLu@gmail.com>
2018-01-03 21:26:34 +0800

    添加文章

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const model = require('../model')
 const Admin = model.getModel('admin')
+const Article = model.getModel('article')
 const Router = express.Router()
 
 Router.post('/login', (req, res) => {
@@ -14,6 +15,20 @@ Router.post('/login', (req, res) => {
       res.json({code: 1, data: doc, mdg: 'suc'})
     }
   })
+})
+
+Router.post('/add', (req, res) => {
+  const {title, author, type, tags, summary, content} = req.body
+  const atags = tags.split(',')
+  Article.create(
+    {title, author, type, summary, content, tags: atags},
+    (err, doc) => {
+      if (err) {
+        res.json({code: 0, msg: err})
+      }
+      res.json({code: 1, msg: 'suc'})
+    }
+  )
 })
 
 module.exports = Router
